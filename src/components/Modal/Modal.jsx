44cedffd053e16f7f4faa9d75f3f3871b677e6d7
Leone510 <meteorologsio@gmail.com>
2022-02-17 14:12:39 +0200

    vanille-tilt

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,22 @@
-import styles from "./Modal.module.scss"
+import React from 'react';
+import Title from "react-vanilla-tilt";
+import styles from "./Modal.module.scss";
+
+const tiltOptions = { 
+   scale: 1, 
+   max: 10, 
+   speed: 200,
+   glare: true,
+   "max-glare": 1
+}
 
 export const Modal = () => {
 
    return (
-      <div className={styles.modal}>
-         <h1>TEST</h1>
-      </div>
+      <Title className="tilt" options={tiltOptions}>
+         <div className={styles.modal}>
+            <h1>TEST</h1>
+         </div>
+      </Title>
    )
 }
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,13 +1,10 @@
-import { AuthWrapper } from "../../components/Auth/AuthWrapper/AuthWrapper"
 import { Modal } from "../../components/Modal/Modal"
 
 export const Registration = () => {
 
    return (
       <div>
-         <AuthWrapper>
-            <Modal/>
-         </AuthWrapper>
+         <Modal/>
       </div>
    )
 }
