21b62126c5d3d82f085e798e48d7326041e92c14
Leone510 <meteorologsio@gmail.com>
2022-11-26 14:50:26 +0200

    input errors style

--- src/components/PostImgWrapper/PostImgWrapper.jsx
+++ src/components/PostImgWrapper/PostImgWrapper.jsx
@@ -1,9 +1,15 @@
 import styles from "./PostImgWrapper.module.scss";
 
-export const PostImageWrapper = ({children}) => {
+export const PostImageWrapper = ({error, children}) => {
    return (
-      <div className={styles.postImageWrapper}>
-         {children}
-      </div>
+      <>
+         <div className={styles.postImageWrapper}>
+            {children}
+         </div>
+
+         <h5 className={styles.error}>
+            {error}
+         </h5>
+      </>
    )
 }
