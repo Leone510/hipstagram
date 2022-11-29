67e0ba980cbe84f2a3719fb6e717ff529c02cf5d
Leone510 <meteorologsio@gmail.com>
2022-11-29 12:07:14 +0200

    change api variables on api function

--- src/api/index.js
+++ src/api/index.js
@@ -1,70 +1,91 @@
 import axios from "axios";
-import { UseLogout } from "../customHooks/UseLogout";
+import { useLogout } from "../customHooks/UseLogout";
 import store from '../store';
 
-const apiJson = axios.create({
-   baseURL: 'https://hipstagram-api.herokuapp.com',
-   headers: {
-      'Content-Type': 'application/json',
-      'Authorization': '',
-   }
-})
+const api = (type = 'json') => {
 
-const apiFormData = axios.create({
-   baseURL: 'https://hipstagram-api.herokuapp.com',
-   headers: {
-      'Content-Type': 'multipart/form-data',
-      'Authorization': '',
+   const contentType = {
+      json: 'application/json',
+      form: 'multipart/form-data',
    }
-})
+   
+   const api = axios.create({
+      baseURL: 'https://hipstagram-api.herokuapp.com',
+      headers: {
+         'Content-Type': contentType[type.toLowerCase()],
+         'Authorization': '',
+      }
+   });
 
-apiJson.interceptors.request.use(config => {
-   config.headers['Authorization'] = store.getState().auth.token;
-   return config;
-})
+   api.interceptors.request.use(config => {
+      config.headers['Authorization'] = store.getState().auth.token;
+      return config;
+   })
+   
+   api.interceptors.response.use((response) => {
+      return response.data;
+   }, (error) => {
+      if (error.response.status === 401 || 400) {
+         console.log('Interceptor error 400/401');
+         
+         const logout = useLogout();
+         logout();
+         return;
+      }
+      console.log('iterceptors error:', error);
+      const lol = Promise.reject(error);
+      return lol;
+   });
+   
+   return api;
+}
 
-apiJson.interceptors.response.use((response) => {
-   return response.data;
-}, (error) => {
-   if (error.response.status === 401) {
-      const logout = UseLogout();
-      logout();
-      return;
-   }
-   console.log('iterceptors error:', error);
-   const lol = Promise.reject(error);
-   return lol;
-});
+// const apiJson = axios.create({
+//    baseURL: 'https://hipstagram-api.herokuapp.com',
+//    headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': '',
+//    }
+// })
+
+// const apiFormData = axios.create({
+//    baseURL: 'https://hipstagram-api.herokuapp.com',
+//    headers: {
+//       'Content-Type': 'multipart/form-data',
+//       'Authorization': '',
+//    }
+// })
+
 
 export const registrationUserRequest = (data) => {
-   return apiJson.post(
+   return api('json').post(
       '/auth/registration', 
       data,
    )
 }
 
 export const loginUserRequest = (data) => {
-   return apiJson.post(
+   return api('json').post(
       '/auth/login',
       data,
    )
 }
 
 export const getCurrentUser = (data) => {
-   return apiJson.get(
+   return api('json').get(
       '/users/current',
       data,
    )
 }
 
 export const setUser = (data) => {
-   return apiJson.patch(
+   return api('json').patch(
       '/users/current',
       data,
    )
 }
 export const createPost = (data) => {
-   return apiFormData.post(
+   return api('form').post(
       '/posts',
       data
    )
